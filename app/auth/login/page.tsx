4cdde0204ca99e773adda295ec9426700b191753
Marius Lefter <160734137+mariussde@users.noreply.github.com>
2025-04-04 11:14:40 +0200

    ff

--- app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "./login-form"
-
-export default function Login() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <LoginForm />
-    </div>
-  )
-} 
